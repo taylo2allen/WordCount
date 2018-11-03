c38949e0967b202c9b84a15dcc1217abed258f55
acash315 <drudollaz2003@yahoo.com>
2018-11-03 12:42:14 -0700

    avl tree work in progress

--- WordCountProject/AVLTree.java
+++ WordCountProject/AVLTree.java
@@ -1,5 +1,11 @@
-
-
+/* Project:      WordCountProject
+// Module:       Correlator.java
+// Contributors: Taylor Allen, Sam Hendryx, Andrew Cash
+// Date:         10/19/18
+ Purpose:      The command line form for Correlator will be as follows:java Correlator -a [-frequency | -unique] <filename2>
+                  -a    Use an AVL Tree
+                 Uses a function to correlate two documents
+ */
 public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E> implements DataCounter<E>{
 	AVLNode overallRoot;
 
@@ -7,7 +13,8 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
 		public AVLNode left;
 		public AVLNode right;
 		public int height;
-
+		//private boolean balanced = true;
+		
 		public AVLNode(E data) {
 			super(data);
 			height = 0;
@@ -49,6 +56,7 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
                     currentNode = currentNode.right;
                     //System.out.println(currentNode.height);
                 }
+                isBalanced();
             }
         }
 	}
@@ -62,5 +70,71 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
 		}
 		return idx;
 	}
-
+	public void isBalanced() {
+		isBalanced(overallRoot);
+		//if it isn't balanced, fix
+		//TODO::Switch statement to determine which case it is if tree is unbalanced.
+		//cases already implemented
+		
+		
+		
+		
+		
+		
+		
+	}
+	//this method checks if the Tree is balanced
+	private int isBalanced(AVLNode root) {		
+		if(root==null) {
+			return -1;
+		}
+		if(root!=null) {
+			int htRight = isBalanced(root.right);
+			int htLeft = isBalanced(root.left);
+			if(Math.abs(height(root.left)-height(root.right))>1||
+					height(root.left) != htLeft || height(root.right) 
+					!= htRight) {
+				System.out.println("This section should never execute");
+			}
+		}
+		return height (root);
+	}   
+	private int height( AVLNode currentNode )
+    {
+        return currentNode == null ? -1 : currentNode.height;
+    }
+	public boolean isEmpty() {
+		
+		return overallRoot==null;
+	}
+	
+	/*	 * Rotations	 */
+	private AVLNode rotateLeft(AVLNode k2) //case 1
+    {
+        AVLNode k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k2.height = Math.max( height( k2.left ), height( k2.right ) ) + 1;
+        k1.height = Math.max( height( k1.left ), k2.height ) + 1;
+        return k1;
+    }    
+    private AVLNode doubleLeft( AVLNode k3 )// case 2
+    {
+        k3.left = rotateRight( k3.left );
+        return rotateLeft( k3 );
+    }
+    private AVLNode doubleRight( AVLNode k1 )//case 3
+    {
+        k1.right = rotateLeft( k1.right );
+        return rotateRight( k1 );
+    }
+    private AVLNode rotateRight(AVLNode k1) //case 4
+    {
+        AVLNode k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+        k1.height = Math.max( height( k1.left ), height( k1.right ) ) + 1;
+        k2.height = Math.max( height( k2.right ), k1.height ) + 1;
+        return k2;
+    }
 }
