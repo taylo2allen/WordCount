2ca32fa32a05635bbd6c63d1d0142f3c6933d020
SHendryx <samuelhendryx@csus.edu>
2018-11-07 13:36:15 -0800

    Working on HashTable.

--- WordCountProject/HashTable.java
+++ WordCountProject/HashTable.java
@@ -6,27 +6,21 @@
 */
 
 public class HashTable implements DataCounter<String>{
-    protected int size;
-    private int key;
-    private int value;
+    protected int size;							//Size of data elements NOT the size of the array
+    public int key;								//Ket for hashtable (created by hashcode)
+    public DataCount<String> table[];			//Array of DataCount<String>. Should hold our Word/Count pairs
 
-    HashTable(int key, int value){
-        this.key = key;
-        this.value = key;
-    }
+//    public String data;
+//    public int count;
+
 
     /** {@inheritDoc} */
     @Override
     public DataCount<String>[] getCounts(){
-        return null;
-    }
-
-    public int getKey(){
-        return key;
-    }
-
-    public int getValue(){
-        return value;
+		@SuppressWarnings("unchecked")
+		DataCount<String>[] counts = new DataCount[size];
+		//Go through HashTable and create an Array of DataCount<String>[]
+		return counts;
     }
 
     /** {@inheritDoc} */
@@ -38,11 +32,21 @@ public class HashTable implements DataCounter<String>{
     /** {@inheritDoc} */
     @Override
     public void incCount(String data){
-
-    }
-
-    public void getArray(){
-
+		//Get HashCode of String "data". HashCode(data) -> key
+		//Check to see if "key" is empty.
+			//If Key is empty, then the word isn't in the table.
+			//Is load factor > 1/2?
+				//YES - resize array (double the siza and find the next prime)
+					//Rehash ALL previously stored data.
+				//NO - Do nothing special
+				//Put the word into the table
+				//Increment size (size++;) (we need to keep track of how many unique entries there are for the output
+		//If the key is NOT empty, (Will a WHILE loop work here?)
+			//Is the data in this location the same word?
+				//YES - increment the data count
+					//Break out of loop
+				//NO - Increment the key
+					//Loop again
     }
 
     private static boolean isPrime(int n){
