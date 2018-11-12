16af620675beaa1bae5b1fac6cde3abd2a19d697
taylo2allen <taylo2allen@gmail.com>
2018-11-12 11:07:56 -0800

    More updates.

--- WordCountProject/HashTable.java
+++ WordCountProject/HashTable.java
@@ -8,8 +8,8 @@
 import static java.lang.Math.pow;
 
 public class HashTable implements DataCounter<String>{
-    protected int size;							      //Size of data elements NOT the size of the array
-    //public int key;				      				//Ket for hashtable (created by hashcode)
+    protected int size;                   //Size of data elements NOT the size of the array
+    //public int key;                     //Ket for hashtable (created by hashcode)
     public DataCount<String>[] table;			//Array of DataCount<String>. Should hold our Word/Count pairs
 
 //    public String data;
@@ -18,10 +18,10 @@ public class HashTable implements DataCounter<String>{
     /** {@inheritDoc} */
     @Override
     public DataCount<String>[] getCounts(){
-		@SuppressWarnings("unchecked")
-		DataCount<String>[] counts = new DataCount[size];
-		//Go through HashTable and create an Array of DataCount<String>[]
-		return counts;
+    @SuppressWarnings("unchecked")
+    DataCount<String>[] counts = new DataCount[size];
+    //Go through HashTable and create an Array of DataCount<String>[]
+    return counts;
     }
 
     /** {@inheritDoc} */
@@ -33,24 +33,24 @@ public class HashTable implements DataCounter<String>{
     /** {@inheritDoc} */
     @Override
     public void incCount(String data){
-		int key = 0;
+    int key = 0;
         //Get HashCode of String "data". HashCode(data) -> key
         key = myHash(data, getSize());
         //Check if table[key] == null
         if (table[key] == null){
-            DataCount<String>[] temp = new DataCount[size.length * 2];
-            for(DataCount c : counts){
-                if(c != null){
-                    key = c.myHash(c.word, temp.lenght);
-                    temp[myHash] = c;
-                }
-            }
             //Prepare insert
             //Is the load factor > 1/2
             if ((double)(getSize()/table.length) >= .5){
                 //YES - double the size of the array to the next prime number
-
+                DataCount<String>[] temp = new DataCount[size.length * 2];
+                for(DataCount c : counts){
+                    if(c != null){
+                        key = c.myHash(c.word, temp.length);
+                        temp[myHash] = c;
+                    }
+                }
             }
+
         }
 
         /*
@@ -89,32 +89,31 @@ public class HashTable implements DataCounter<String>{
                     hash = hash mod entrys.length
 
 
-        class Entry
-            int count
-            String word
+            class Entry
+                int count
+                String word
 
-            Entry(String s)
-                this.word = s
-                count = 1
-
+                Entry(String s)
+                    this.word = s
+                    count = 1
          */
 
 
 
-		//Check to see if "key" is empty.
-			//If Key is empty, then the word isn't in the table.
-			//Is load factor > 1/2?
-				//YES - resize array (double the siza and find the next prime)
-					//Rehash ALL previously stored data.
-				//NO - Do nothing special
-				//Put the word into the table
-				//Increment size (size++;) (we need to keep track of how many unique entries there are for the output
-		//If the key is NOT empty, (Will a WHILE loop work here?)
-			//Is the data in this location the same word?
-				//YES - increment the data count
-					//Break out of loop
-				//NO - Increment the key
-					//Loop again
+    //Check to see if "key" is empty.
+      //If Key is empty, then the word isn't in the table.
+      //Is load factor > 1/2?
+        //YES - resize array (double the siza and find the next prime)
+          //Rehash ALL previously stored data.
+        //NO - Do nothing special
+        //Put the word into the table
+        //Increment size (size++;) (we need to keep track of how many unique entries there are for the output
+    //If the key is NOT empty, (Will a WHILE loop work here?)
+      //Is the data in this location the same word?
+        //YES - increment the data count
+          //Break out of loop
+        //NO - Increment the key
+          //Loop again
     }
 
     private static boolean isPrime(int n){
